099d44a1d80529d32b74e2080809a8a86c5638ef
Santiago Cabello <saicabello@gmail.com>
2023-04-24 11:41:04 -0300

    ultima actualizacion 24/4

--- Trabajo Practico 2.0/src/main/java/org/example/Main.java
+++ Trabajo Practico 2.0/src/main/java/org/example/Main.java
@@ -1,5 +1,4 @@
 package org.example;
-import java.util.ArrayList;
 import java.util.List;
 import java.io.FileNotFoundException;
 import java.sql.*;
@@ -7,9 +6,9 @@ public class Main {
     public static void main(String[] args) throws SQLException, FileNotFoundException {
 
         // Listas para almacenar los equipos, partidos y pronosticos
-        List<Equipo> equipos = new ArrayList<>();
-        List<Partido> partidos = new ArrayList<>();
-        List<Pronostico> listaPronosticos = new ArrayList<>();
+
+        List<Partido> partidos ;
+        List<Pronostico> listaPronosticos ;
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
         Ronda ron = new Ronda(); //CREAMOS RONDA
         partidos= ron.leerDatosRonda("resultadopartidos"); //METEMOS EN LA LISTA PARTIDOS LOS PARTIDOS Y RESULTADOS LEIDOS DE LA BASE DE DATOS
@@ -21,6 +20,8 @@ public class Main {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 
         int puntosTotal=0;
+
+
         if(partidos.size() == listaPronosticos.size()) {
             for (int i = 0; i < listaPronosticos.size(); i++) { //PRIMER RECORRIDO ANALIZANDO LOS PRONOSTICOS Y RESULTADOS
 
@@ -28,8 +29,8 @@ public class Main {
                 san.getPronostico().get(i).setPartido(partidos.get(i)); //AGREGA PARTIDO A LOS PROnOSTICOS DEL PARTICIPANTE
 
 
-                System.out.println("------------------------------------------------------------------------------------");
-                System.out.println("Partido Entre: " + san.getPronostico().get(i).getPartido().partidoEntre()); //IMPRIME EL STRING DEL PARTIDO APOSTADO
+                System.out.println("---------------------------------------- Ronda "+ ron.getNumeroRonda() +"--------------------------------------------");
+                System.out.println("Partido Entre: " + san.getPronostico().get(i).partidoAPronosticar()); //IMPRIME EL STRING DEL PARTIDO APOSTADO
                 System.out.println("Resultado: " +  san.getPronostico().get(i).getPartido().resultadoPartido());//IMPRIME EL STRING DEL RESULTADO DE PARTIDO APOSTADO
                 System.out.println("Apuesta de Participante: gana local? [" + san.getPronostico().get(i).isGanaEquipoLocal() //IMPRIME LA APUESTA DEL PARTICIPANTE
                         + "] gana visitante? [" + san.getPronostico().get(i).isGanaEquipoVisitante() + "] o empatan? [" +
@@ -41,11 +42,45 @@ public class Main {
             }
         }
         if (puntosTotal == partidos.size()){//SI EL PARTICIPANTE ACIERTA TODOS LOS PARTIDOS SE DUPLICAN LOS PUNTOS TOTALES
+            System.out.println("acertò todos los partidos, duplica puntos !!");
             puntosTotal= puntosTotal * 2;
         }
-        System.out.println("Puntos totales del Jugador: -------------- "+ puntosTotal+ " --------------");
+        System.out.println("Puntos totales del Jugador hasta la ronda "+ron.getNumeroRonda()+  ":     ---- "+ puntosTotal+ " ----");
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------
+        List<Partido> partidosRonda2 ;
+        List<Pronostico> listaPronosticos2;
+
+        partidosRonda2 = ron.leerDatosRonda("resultadopartidos2"); //METEMOS EN LA LISTA PARTIDOS LOS PARTIDOS Y RESULTADOS LEIDOS DE LA BASE DE DATOS
+
+        listaPronosticos2 = pro.leerDatosPronostico("apuesta2");// METEMOS EN LA LISTA DE PRONOSTICOS LOS PRONOSTICOS DE UN PARTICIPANTE
+
+        //--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        if(partidosRonda2.size() == listaPronosticos2.size()) {
+            for (int i = 0; i < listaPronosticos2.size(); i++) {
+
+                pro.setPartido(partidosRonda2.get(i));// AGREGA PARTIDO A LA LISTA DE PRONOSTICO
+                san.getPronostico().get(i).setPartido(partidosRonda2.get(i)); //AGREGA PARTIDO A LOS PRONOSTICOS DEL PARTICIPANTE
 
 
+                System.out.println("---------------------------------------- Ronda "+ ron.getNumeroRonda() +"--------------------------------------------");
+                System.out.println("Partido Entre: " + san.getPronostico().get(i).partidoAPronosticar()); //IMPRIME EL STRING DEL PARTIDO APOSTADO
+                System.out.println("Resultado: " +  san.getPronostico().get(i).getPartido().resultadoPartido());//IMPRIME EL STRING DEL RESULTADO DE PARTIDO APOSTADO
+                System.out.println("Apuesta de Participante: gana local? [" + san.getPronostico().get(i).isGanaEquipoLocal() //IMPRIME LA APUESTA DEL PARTICIPANTE
+                        + "] gana visitante? [" + san.getPronostico().get(i).isGanaEquipoVisitante() + "] o empatan? [" +
+                        san.getPronostico().get(i).isEmpatan() + "]");
+                System.out.println("Suma " + san.getPronostico().get(i).puntos() + " Puntos");//INVOCA AL METODO PUNTOS() QUE SUMA 1 PUNTO SI GANA EL EQUIPO QUE APOSTO EL PARTICIPANTE
+
+                puntosTotal = puntosTotal + san.getPronostico().get(i).puntos();//ACUMULADOR DE PUNTOS
+
+            }
+        }
+        if (puntosTotal == partidos.size()){//SI EL PARTICIPANTE ACIERTA TODOS LOS PARTIDOS SE DUPLICAN LOS PUNTOS TOTALES
+            System.out.println("acertò todos los partidos, duplica puntos !!");
+            puntosTotal= puntosTotal * 2;
+        }
+        System.out.println("Puntos totales del Jugador hasta la ronda "+ron.getNumeroRonda()+  ":      ----"+ puntosTotal+ " ----");
 
 
     }
